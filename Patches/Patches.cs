d72ca8ea9ccda7a2efaec5db0172cd7917761223
Jakey08044 <jake080455@gmail.com>
2026-05-22 04:54:24 -0400

    1

---
+++ Patches/Patches.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using EFT;
+using HarmonyLib;
+using SPT.Reflection.Patching;
+using System.Reflection;
+using Comfort.Common;
+using EFT.InventoryLogic;
+using UnityEngine;
+
+namespace Butterfingers.Patches;
+
+public class Patches
+{
+    
+    static IEnumerator WaitThenDrop(Player player)
+    {
+        //God forbid I try to use OnDropWeapon without waiting this took me way too long to figure out
+        yield return new WaitForSeconds(0.1f);
+        player.HandsController.OnDropWeapon();
+    }
+    
+   
+    internal class ButterfingersP : ModulePatch
+    {
+        protected override MethodBase GetTargetMethod()
+        {
+            return AccessTools.Method(typeof(Player.FirearmController.GClass2029), nameof(Player.FirearmController.GClass2029.InternalOnFireEvent));
+        }
+        [PatchPostfix]
+        static void Postfix(Player.FirearmController.GClass2029 __instance)
+        {
+            
+            
+            var state = __instance.Weapon_0.MalfState.State;
+            if (state != Weapon.EMalfunctionState.None)
+            { 
+                if (Random.Range(0f, 1f) <= Plugin.DropChance.Value)
+                {
+                    Plugin.Instance.StartCoroutine(WaitThenDrop(__instance.Player_0));   
+                }
+            }
+            
+        }
+    }
+}
---
+++ Plugin.cs
@@ -0,0 +1,33 @@
+﻿using BepInEx;
+using BepInEx.Configuration;
+using BepInEx.Logging;
+using Butterfingers.Patches;
+namespace Butterfingers
+{
+    [BepInPlugin("Butterfingers.j08044", "Butterfingers", "1.0.0")]
+    public class Plugin : BaseUnityPlugin
+    {
+        public static ManualLogSource LogSource;
+        public static Plugin Instance;
+        public static ConfigEntry<float> DropChance;
+
+        // BaseUnityPlugin inherits MonoBehaviour, so you can use base unity functions like Awake() and Update()
+        private void Awake()
+        {
+            LogSource = Logger;
+            LogSource.LogInfo("plugin loaded!");
+            Instance = this;
+            DropChance = Config.Bind(
+                "General",        // section
+                "Gun Drop Chance", // key
+                0.25f,            // default value
+                new ConfigDescription(
+                    "Chance of your gun dropping on malfunction (0.0 - 1.0) Ex: 0.25 = 25% chance of gun dropping",
+                    new AcceptableValueRange<float>(0f, 1f)
+                )
+            );
+            
+            new Patches.Patches.ButterfingersP().Enable();
+        }
+    }
+}
